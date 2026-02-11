188d718b8d93f5539c60273c15b1ca8c33d6b447
leihaohao <2468048176@qq.com>
2026-02-11 17:56:53 +0800

    chore(shared): 增加全局配置类型定义

--- src/shared/components/section-layout/index.ts
+++ src/shared/components/section-layout/index.ts
@@ -3,3 +3,14 @@ export { default as SectionMain } from './components/main.vue'
 export { default as SectionLayout } from './index.vue'
 
 export type * from './interface'
+
+declare module '@vue-spark/compose-kit-runtime' {
+  interface GlobalConfig {
+    SectionLayout?: {
+      /**
+       * 内置卡片样式类名，方便统一设置卡片样式
+       */
+      cardClass?: any
+    }
+  }
+}
